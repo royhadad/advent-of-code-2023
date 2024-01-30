e8f39678177a2ce08a58a1301152a488a6363c7f
Roy Hadad <roy@datree.io>
2024-01-30 15:34:01 +0800

    wip: day 4 part 2, we know what needs to be done, but the code is not ready yet

---
+++ days/day4/linearEquation.ts
@@ -0,0 +1,33 @@
+import * as math from "mathjs";
+
+export interface LinearEquation {
+  coefficients: number[]; // Coefficients of the unknowns (n + 6)
+  constant: number; // Constant on the right-hand side
+}
+
+interface Solution {
+  unknowns: number[]; // The solution for the unknowns
+}
+
+export function solveSystemOfEquations(equations: LinearEquation[]): Solution | null {
+  try {
+    const coefficientsMatrix = equations.map((eq) => eq.coefficients);
+    const constants = equations.map((eq) => [eq.constant]);
+
+    // Use math.matrix to create a matrix from the coefficients array
+    const augmentedMatrix = math.matrix(coefficientsMatrix);
+    const augmentedConstants = math.matrix(constants);
+
+    // Convert to array before calling lusolve
+    const solutionArray = math.lusolve(augmentedMatrix, augmentedConstants);
+
+    const results: number[] = [];
+    solutionArray.forEach((x) => {
+      results.push(x[0]);
+    });
+    return { unknowns: results };
+  } catch (error) {
+    console.error("Error solving system of equations:", error);
+    return null;
+  }
+}
--- days/day4/part2.ts
+++ days/day4/part2.ts
@@ -1,5 +1,6 @@
 import fs from "fs";
 import { parseInput, Hailstone } from "./parser";
+import { LinearEquation, solveSystemOfEquations } from "./linearEquation";
 
 const input = fs.readFileSync(__dirname + "/input.txt", "utf-8").trim();
 
@@ -7,10 +8,125 @@ console.log("started");
 
 const hailstones = parseInput(input);
 
-hailstones.sort((hs1: Hailstone, hs2: Hailstone) => {
-  return hs2.position.x - hs1.position.x;
-});
 
-console.log(hailstones);
+// Convert each hailstone to a linear equation
+const linearEquations: LinearEquation[] = hailstones.map(hailstoneToEquation);
+
+const solution = solveSystemOfEquations(linearEquations);
+
+if (solution !== null) {
+  console.log("Solution found:", solution);
+} else {
+  console.log("No solution found.");
+}
 
 console.log("done");
+
+
+// dubious, created by chatGPT3.5
+// Convert Hailstone to LinearEquation
+function hailstoneToEquation(hailstone: Hailstone): LinearEquation {
+  const { position, velocity } = hailstone;
+  return {
+    coefficients: [
+      1, 0, 0, 0, 0, 0,        // px
+      -velocity.x, 0, 0, 0, 0,  // -vx
+      0, 1, 0, 0, 0, 0,         // py
+      0, -velocity.y, 0, 0, 0,  // -vy
+      0, 0, 1, 0, 0, 0,         // pz
+      0, 0, -velocity.z, 0, 0,  // -vz
+    ],
+    constant: position.x - velocity.x * position.x,
+  };
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// the hailstones
+// x1=px1+t1*vx1
+// y1=py1+t1*vy1
+
+// x2=px2+t2*vx2
+// y2=py2+t2*vy2
+
+// the stone
+// x=px+t*vx
+// y=py+t*vy
+
+// find this
+// px+t1*vx = px1+t1*vx1
+// py+t1*vy = py1+t1*vy1
+
+// px+t2*vx = px2+t2*vx2
+// py+t2*vy = py2+t2*vy2
+
+// and with n hailstones:
+// px+t1*vx = px1+t1*vx1
+// y=py+t1*vy = py1+t1*vy1
+
+// px+t2*vx = px2+t2*vx2
+// py+t2*vy = py2+t2*vy2
+
+// px+t2*vx = px2+t2*vx2
+// py+t2*vy = py2+t2*vy2
+
+// px+t3*vx = px3+t3*vx3
+// py+t3*vy = py3+t3*vy3
+
+// ...
+
+// px+tn*vx = pxn+tn*vxn
+// py+tn*vy = pyn+tn*vyn
+
+// ------------------------------
+
+// and with 3 dimensions:
+// px+t1*vx = px1+t1*vx1
+// py+t1*vy = py1+t1*vy1
+// pz+t1*vz = pz1+t1*vz1
+
+// px+t2*vx = px2+t2*vx2
+// py+t2*vy = py2+t2*vy2
+// pz+t2*vz = pz2+t2*vz2
+
+// px+t2*vx = px2+t2*vx2
+// py+t2*vy = py2+t2*vy2
+// pz+t2*vz = pz2+t2*vz2
+
+// px+t3*vx = px3+t3*vx3
+// py+t3*vy = py3+t3*vy3
+// pz+t3*vz = pz3+t3*vz3
+
+// ...
+
+// px+tn*vx = pxn+tn*vxn
+// py+tn*vy = pyn+tn*vyn
+// pz+tn*vz = pzn+tn*vzn
+
+// we have n+6 unknowns
+// we have 3n equations
+// we need to solve 3n equations with n+6 unknowns
+
+// 
