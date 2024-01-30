48341dafc47acce0be596c333278efe66c3a662b
Roy Hadad <roy@datree.io>
2024-01-30 12:14:12 +0800

    this might work, keeping this version

--- days/day4/part1.ts
+++ days/day4/part1.ts
@@ -113,18 +113,6 @@ function findIntersectionPoint(positionsAndVelocities: {
   // py1+vy1*((px2+vx2*t2 - px1)/vx1) = py1+vy1*t2
 
   // py1 + (vy1*(px2+vx2*t2 - px1))/(vy1*vx1) = py1+vy1*t2
-
-  // py1 + (vy1*(px2+vx2*t2 - px1))/(vy1*vx1) = py1+vy1*t2
-
-  //
-
-  const line2 = (x) => vy1 * (x - px1) + py1;
-  const line3 = (x) => vy2 * (x - px2) + py2;
-
-  const x = (py2 + vy1 * px1 - py1 - px2 * vy2) / (vy1 - vy2);
-  const y = vy1 * (x - px1) + py1;
-
-  return { x, y };
 }
 
 // find intersections between hailstones' paths (no collision needed)
@@ -159,3 +147,70 @@ function findIntersectionPoint(positionsAndVelocities: {
 // x1(t) = px1 + -2*t
 // y1(t) = py1 + 1*t
 //
+
+// 19−2t1=18−t2
+// 13+t1=19-t2
+
+// 19−2t1=18−t2
+// 13+t1=19-t2
+//
+
+function findIntersectionPoint(positionsAndVelocities: {
+  px1: number;
+  py1: number;
+  vx1: number;
+  vy1: number;
+  px2: number;
+  py2: number;
+  vx2: number;
+  vy2: number;
+}): { x: number; y: number; isInThePastLine1: boolean; isInThePastLine2: boolean } | null {
+  const { px1, py1, vx1, vy1, px2, py2, vx2, vy2 } = positionsAndVelocities;
+
+  // the functions
+  // const line1X = (t: number) => px1 + t * vx1;
+  // const line1Y = (t: number) => py1 + t * vy1;
+
+  // const line2X = (f: number) => px2 + f * vx2;
+  // const line2Y = (f: number) => py2 + f * vy2;
+
+  // the equations to solve, we need to find the intersection point
+  // px1 + t * vx1 = px2 + f * vx2
+  // py1 + t * vy1 = py2 + f * vy2
+
+  // extract t from first equation
+  // px1 + t * vx1 = px2 + f * vx2
+  // t = (px2 + f * vx2 - px1)/(vx1)
+
+  // substitute t in 2nd equation
+  // py1 + t * vy1 = py2 + f * vy2
+  // py1 + ((px2 + f * vx2 - px1)/(vx1)) * vy1 = py2 + f * vy2
+
+  // solve for f
+  // py1 + ((px2 + f * vx2 - px1)/(vx1)) * vy1 = py2 + f * vy2
+  // f = (vx1 * (py2 - py1) - px2 + px1) / (vx2 - vx1 * vy2)
+
+  // solve for t
+  // t = (px2 + f * vx2 - px1)/(vx1)
+
+  // find x and y
+  // const line1XAtIntersection = line1X(t)
+  // const line1YAtIntersection = line1Y(t)
+
+  if (vx2 - vx1 * vy2 === 0 || vx1 === 0) {
+    return null;
+  }
+
+  const f = (vx1 * (py2 - py1) - px2 + px1) / (vx2 - vx1 * vy2);
+  const t = (px2 + f * vx2 - px1) / vx1;
+
+  const line1XAtIntersection = px1 + t * vx1;
+  const line1YAtIntersection = py1 + t * vy1;
+
+  return {
+    x: line1XAtIntersection,
+    y: line1YAtIntersection,
+    isInThePastLine1: t < 0,
+    isInThePastLine2: f < 0,
+  };
+}
